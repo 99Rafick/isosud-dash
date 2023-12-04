01641c6f92fff366304329745d2d2d5c2f3bed6e
99Rafick <rafraf9727@gmail.com>
2023-12-04 14:03:44 +0100

    chore: update

--- database/migrations/2023_12_04_104030_create_processes_table.php
+++ database/migrations/2023_12_04_104030_create_processes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('process', function (Blueprint $table) {
+        Schema::create('processes', function (Blueprint $table) {
             $table->id();
             $table->longText('name');
             $table->string('category');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('process');
+        Schema::dropIfExists('processes');
     }
 };
