337b03112cd1c0156322770ff1854b124e583a26
99Rafick <rafraf9727@gmail.com>
2023-12-04 12:25:58 +0100

    chore: create Process Enum

---
+++ app/Enums/ProcessEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum ProcessEnum
+{
+    public const CATEGORY = [
+        'management' => 'Management',
+        'realisation' => 'Réalisation',
+        'support' => 'Supports'
+    ];
+}
