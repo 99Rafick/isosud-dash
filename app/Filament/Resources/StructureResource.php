8c95b512460fffdebfe2f761f1f7e28aa3f9fbb8
99Rafick <rafraf9727@gmail.com>
2023-12-04 14:29:01 +0100

    chore: build Structure filament resources

---
+++ app/Filament/Resources/StructureResource.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StructureResource\Pages;
+use App\Filament\Resources\StructureResource\RelationManagers;
+use App\Models\Structure;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StructureResource extends Resource
+{
+    protected static ?string $model = Structure::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Textarea::make('name')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('domain')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('sector')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('logo')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('domain')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('sector')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('logo')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStructures::route('/'),
+            'create' => Pages\CreateStructure::route('/create'),
+            'edit' => Pages\EditStructure::route('/{record}/edit'),
+        ];
+    }    
+}
