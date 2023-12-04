18bbaba678b656b0cc08d9dcf6e7fce3be8b791b
99Rafick <rafraf9727@gmail.com>
2023-12-04 15:43:36 +0100

    chore: create filament Enum

---
+++ app/Enums/FilamentEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum FilamentEnum
+{
+    public const FORM_SECTION_TITLE = 'Information à renseigner';
+
+    public const FORM_SECTION_DESCRIPTION = 'Ces informations sont importantes à renseigner';
+}
