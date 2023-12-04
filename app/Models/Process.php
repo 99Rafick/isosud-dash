d273f7aba73bb80cc9a4e36da326695af1c0ec1c
99Rafick <rafraf9727@gmail.com>
2023-12-04 15:45:27 +0100

    feat: add property fillable and structure relationship method

--- app/Models/Process.php
+++ app/Models/Process.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Process extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+        'category',
+        'structure_id'
+    ];
+    public function structure(): BelongsTo
+    {
+        return $this->belongsTo(Structure::class, 'structure_id');
+    }
 }
