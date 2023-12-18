27d7d953ef4617d40748c529fd85e7715261cf11
99Rafick <rafraf9727@gmail.com>
2023-12-18 10:32:21 +0100

    typo: update Structure Page

--- app/Filament/Resources/UserResource/Widgets/Structures.php
+++ app/Filament/Resources/UserResource/Widgets/Structures.php
@@ -5,8 +5,10 @@ namespace App\Filament\Resources\UserResource\Widgets;
 use App\Enums\RoleEnum;
 use App\Models\Structure;
 use App\Models\User;
+use Filament\Support\Colors\Color;
 use Filament\Tables;
 use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
 
@@ -19,13 +21,30 @@ class Structures extends BaseWidget
         return $table
             ->query(User::whereHas('roles', fn ($query) => $query->where('roles.name', RoleEnum::STRUCTURE)))
             ->columns([
-                Tables\Columns\ImageColumn::make('logo'),
-                Tables\Columns\TextColumn::make('name')->label('Nom'),
-                Tables\Columns\TextColumn::make('sector')->label('Secteur'),
-                Tables\Columns\TextColumn::make('domain')->label('Domaine'),
+
+                Stack::make([
+                    Tables\Columns\ImageColumn::make('logo')
+                        ->extraAttributes(['class' => 'col-span-1 mb-2']),
+                    Tables\Columns\TextColumn::make('Info')
+                        ->getStateUsing(fn($record) => 'Secteur ' . $record->sector . ', Domaine '. $record->domain)
+                        ->badge()
+                        ->color(Color::Gray)
+                        ->extraAttributes([
+                            'class' => 'text-md'
+                        ]),
+                    Tables\Columns\TextColumn::make('name')
+                        ->label('Nom')
+                        ->size(Tables\Columns\TextColumn\TextColumnSize::Large)
+                        ->extraAttributes(['class' => 'mt-3']),
+                ]),
+            ])
+            ->contentGrid([
+                'md' => 2,
+                'xl' => 3,
             ])->actions([
                 Action::make('Processus')
                     ->url('iii')
+                    ->extraAttributes(['style' => 'justify-content: flex-end !important'])
             ]);
     }
 }
