7c33262d459542cde08fb6dda4b5a227c9ecdb58
99Rafick <rafraf9727@gmail.com>
2023-12-18 10:56:31 +0100

    feat: register Process user in terms of role

--- app/Filament/Resources/ProcessResource/Pages/CreateProcess.php
+++ app/Filament/Resources/ProcessResource/Pages/CreateProcess.php
@@ -2,11 +2,23 @@
 
 namespace App\Filament\Resources\ProcessResource\Pages;
 
+use App\Enums\RoleEnum;
 use App\Filament\Resources\ProcessResource;
+use App\Models\Process;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class CreateProcess extends CreateRecord
 {
     protected static string $resource = ProcessResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        if (!RoleEnum::isAdmin()) {
+            $data['user_id'] = Auth::user()->id;
+        }
+        return Process::create($data);
+    }
 }
